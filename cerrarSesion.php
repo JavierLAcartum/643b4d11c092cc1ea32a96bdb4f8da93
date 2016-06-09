9a5eaf4b02048074b44b0a17a5cb00234df85f2a
Sonileo <sonypoke@gmail.com>
2016-06-10 00:39:18 +0200

    cerrar sesión

--- cerrarSesion.php
+++ cerrarSesion.php
@@ -1,4 +1,4 @@
-<?php echo "Estas seguro de que quieres cerrar sesion"; 
+<?php
 	if(session_id() == '') {
 		session_start();
 	}
@@ -9,6 +9,9 @@
 		}
 	?>
 <form id='cerrarSesion' action='<?php echo  $user ?>.php' method='post' enctype="multipart/form-data" accept-charset='UTF-8'>
-	<input type='submit' name="salir" value="Cerrar sesion"/>
+	</br> </br> </br> </br>
+	<label style="margin-left: 510px; font-family:'Segoe UI'; font-size: 20px;"> ¿Estás seguro de que quieres cerrar sesión? </label> 
+	</br> </br>
+	<input type='submit' name="salir" value="Cerrar sesion" style="margin-left: 625px;" />
 	<button onclick="location.href='subastador.php'"> Volver</button>
 </form>
