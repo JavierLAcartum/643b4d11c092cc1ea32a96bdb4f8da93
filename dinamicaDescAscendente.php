51d8fba3bf542cbe9380c810d2c62945dfe491a8
JavierLAcartum <minikey13@gmail.com>
2016-06-10 18:03:20 +0200

    Mecanismo Subasta Descubierta Terminado
    Faltan cosas, pero el funcionamiento está correcto

--- dinamicaDescAscendente.php
+++ dinamicaDescAscendente.php
@@ -21,15 +21,16 @@
 		}
         //$selectPujas = "SELECT id FROM usuarios WHERE id = '".$_SESSION['user']['postor']."'";
         //$resultSubastas = $conn->query($selectSubastas);
-       $select = "SELECT tipo FROM subastas WHERE id='$idSubasta'";
+       $select = "SELECT tipo, precioinicial  FROM subastas WHERE id='$idSubasta'";
        $result = $conn->query($select);
        $row=$result->fetch_assoc();
        $tipoSubasta = $row['tipo'];
+       $precio = $row['precioinicial'];
         $select = "SELECT cantidad FROM pujas WHERE idsubasta='$idSubasta'";
 	   $result = $conn->query($select);
 	   $idUser = '';
         if ($result->num_rows> 0) {
-            $precio=0;
+            //$precio=0;
             while( $row=$result->fetch_assoc()){
                 $precioronda = $row['cantidad'];
                 if($precio<$precioronda && ($tipoSubasta==1 || $tipoSubasta==3)){
@@ -54,16 +55,24 @@
     
 
 //***********************************************************************************//
+if(isset($_POST['puja'])){
 $pujactual = $_POST['puja'];
-if($pujactual > valorMinimo($idSubasta))
-    {
-        $conn = new mysqli("localhost", "643b4d11c092cc1e", "sekret", "643b4d11c092cc1ea32a96bdb4f8da93");
+$conn = new mysqli("localhost", "643b4d11c092cc1e", "sekret", "643b4d11c092cc1ea32a96bdb4f8da93");
 		$selectSubastas;
 		$resultSubastas;
     	if(session_id() == '') {
             session_start();
 		}
-     
+       $select = "SELECT tipo FROM subastas WHERE id='$idSubasta'";
+       $result = $conn->query($select);
+       $row=$result->fetch_assoc();
+       $tipoSubasta = $row['tipo'];
+if($pujactual > valorMinimo($idSubasta)&& ($tipoSubasta==1 || $tipoSubasta==3))
+    {
+        $conn = new mysqli("localhost", "643b4d11c092cc1e", "sekret", "643b4d11c092cc1ea32a96bdb4f8da93");
+		$selectSubastas;
+		$resultSubastas;
+    	  
         $idUser = $_SESSION['user']['postor'];		
                         
         // Then call the date functions
@@ -77,10 +86,28 @@ if($pujactual > valorMinimo($idSubasta))
         } else {
             echo "Error updating record: " . $conn->error;
         }
-    }else{
-        echo "La puja es inferior a la establecida!";
-    }
-    
+}else if($pujactual < valorMinimo($idSubasta)&& ($tipoSubasta==2 || $tipoSubasta==4)){
+        $conn = new mysqli("localhost", "643b4d11c092cc1e", "sekret", "643b4d11c092cc1ea32a96bdb4f8da93");
+		$selectSubastas;
+		$resultSubastas;
+    	  
+        $idUser = $_SESSION['user']['postor'];		
+                        
+        // Then call the date functions
+        $date = date('Y-m-d H:i:s');
+        // Or
+        $date = date('Y/m/d H:i:s');
+        $puja = $_POST['puja'];
+        $select = "INSERT INTO pujas (fecha, cantidad, idsubasta, idpostor) VALUES ('$date', '$puja',    '$idSubasta', '$idUser')";
+        if ($conn->query($select) === TRUE) {
+            echo "Usuario Puja Correcta.";
+        } else {
+            echo "Error updating record: " . $conn->error;
+        }
+}else{
+    echo "La puja tiene un valor incorrecto!";
+}
+}
 
 ?>
     <html>
@@ -93,7 +120,7 @@ if($pujactual > valorMinimo($idSubasta))
                     <button name='submit'>Puja</button>
                 </form>
             </a>
-        
+
         <script type="text/javascript">
             //setInterval(function(){ refreshTable(); }, 3000);
             //setTimeout(refreshTable, 5000);
@@ -101,31 +128,30 @@ if($pujactual > valorMinimo($idSubasta))
             /*function refreshTable() {
                 $('#tableHolder').load('listaPujas.php', function());
             }*/
-                                       
-                                       
-        function loadDoc() {
-           
-          var xhttp = new XMLHttpRequest();
-            console.log(xhttp.status);
-            xhttp.onreadystatechange = function() {
-            if ((xhttp.readyState == 4) && (xhttp.status == 200)) {
-                
-             document.getElementById("demo").innerHTML = xhttp.responseText;
+
+
+            function loadDoc() {
+
+                var xhttp = new XMLHttpRequest();
+                console.log(xhttp.status);
+                xhttp.onreadystatechange = function () {
+                    if ((xhttp.readyState == 4) && (xhttp.status == 200)) {
+
+                        document.getElementById("demo").innerHTML = xhttp.responseText;
+                    }
+                };
+                xhttp.open("GET", "listaPujas.php?id=<?php echo $idSubasta; ?>", true);
+                xhttp.send();
             }
-          };
-                    xhttp.open("GET", "listaPujas.php?id=<?php echo $idSubasta; ?>", true);
-                    xhttp.send();
-        }
-                                       
-        setInterval(function(){
-            loadDoc();
-        }, 500);
-            
+
+            setInterval(function () {
+                loadDoc();
+            }, 500);
         </script>
-        
-        
+
+
         <div id="demo"></div>
-        
+
         <div id="tableHolder"></div>
     </body>
 
