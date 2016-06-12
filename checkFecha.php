19eeadf9fcb21f30bec57d0b395f692ffbf76a33
JavierLAcartum <minikey13@gmail.com>
2016-06-12 02:53:51 +0200

    Holandesa Casi terminada
    La subasta holandesa esta casi terminada, solo queda reconocer bien el
    tiempo para que cambie el precio. Solo hay un problema... Si el cliente
    cierra la pagina de la puja, deja de meterse en la funcion de checkear y
    por consiguiente no cambia el precio. Asi que esta funcion tiene que
    ejecutarse SIEMPRE en el servidor. ¿Cómo? Pues tengo sueño asi que no me
    ha dado para pensarlo espero que por la mañana este mas lucido.

--- checkFecha.php
+++ checkFecha.php
@@ -14,4 +14,36 @@ function RedirectToURL($url, $tiempo)
     header("Refresh: $tiempo, URL=$url");
     exit;
 }
+$select = "SELECT precioinicial, cambioprecio, precioactual, tipo FROM subastas WHERE id='$idSubasta'";
+$result = $conn->query($select);
+$row = $result->fetch_assoc();
+$precioInicial = $row['precioinicial'];
+$sumar = $row['cambioprecio'];
+$precioActual=$row['precioactual'];
+$tipoSubasta=$row['tipo'];
+if($precioActual==null){
+    if(tipo == 5){
+        $precioActual = $precioInicial+$sumar;
+    }else if(tipo == 6){
+        $precioActual = $precioInicial-$sumar;
+    }
+   
+}else{
+    if(tipo == 5){
+        $precioActual = $precioActual+$sumar;
+    }else if(tipo == 6){
+        $precioActual = $precioActual-$sumar;
+    }
+}
+
+$update= "UPDATE subastas SET precioactual='$precioActual' WHERE id='$idSubasta'";
+
+		
+if ($conn->query($update) === TRUE) {
+    echo $precioActual;
+    return true;
+} else {
+    echo "Error updating record: " . $conn->error;
+    return false;
+}
 ?>
