16d8742ade65d7a255d54ebacf943bbb91e62528
Sonileo <sonypoke@gmail.com>
2016-06-13 00:18:30 +0200

    .

--- valorMinimo.php
+++ valorMinimo.php
@@ -86,15 +86,15 @@
        $row=$result->fetch_assoc();
        $tipoSubasta = $row['tipo'];
        $precio = $row['precioinicial'];
-<<<<<<< HEAD
+
 	     $idPujaRetorno;
        $select = "SELECT cantidad, id FROM pujas WHERE idsubasta='$idSubasta'";
 	     $result = $conn->query($select);
-=======
+
 	   $idPujaRetorno;
        $select = "SELECT cantidad, id FROM pujas WHERE idsubasta='$idSubasta'";
 	   $result = $conn->query($select);
->>>>>>> origin/master
+¡
         if ($result->num_rows> 0) {
             
             while( $row=$result->fetch_assoc()){
