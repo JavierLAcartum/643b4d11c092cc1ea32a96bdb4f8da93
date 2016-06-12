edb0f0f7bd3f41625c07f0605616012beeae10ed
Sonileo <sonypoke@gmail.com>
2016-06-12 12:03:30 +0200

    Registro

--- registro.php
+++ registro.php
@@ -1,6 +1,6 @@
 <form action="index.php" method="post">
 
-	<input type='text' name='username' id='username' placeholder="Usuario" maxlength="20" style="width:100px; margin-left: 580px; margin-top: 300px;" required /> <br/><br/>
+	<input type='text' name='username' id='username' placeholder="Usuario" maxlength="20" style="width:100px; margin-left: 580px; margin-top: 400px;" required /> <br/><br/>
 	<input type='password' name='password' id='password' placeholder="Password" maxlength="20" style="width:100px; margin-left: 580px;" required /> <br/> <br/>
 	
 	<input type='text' name='nombre' id='nombre' placeholder="Nombre" maxlength="20" style="width:100px; margin-left: 580px;" required /> <br/><br/>
